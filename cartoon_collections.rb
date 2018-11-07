0eab97cfddb37552ea6b53d6779cba45958c2a56
Maharshee roy <m.roy@optonline.net>
2018-11-07 00:33:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,6 +6,7 @@ end
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
   planeteer_calls.collect do |x|
+    x.
 end
 
 def long_planeteer_calls# code an argument here
