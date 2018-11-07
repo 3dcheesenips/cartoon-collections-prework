c6ca698610e1014bc1be7a0464d501e558f103aa
Maharshee roy <m.roy@optonline.net>
2018-11-07 00:36:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  words.any? do |x|
+  words.any? do |word|
+    word.length > 4
 end
 
 def find_the_cheese# code an argument here
