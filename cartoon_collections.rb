7ae1a497d73e7ff0b357969c54ce74be1d9ae6de
Maharshee roy <m.roy@optonline.net>
2018-11-07 00:38:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
+  array.find do |cheeses|
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
