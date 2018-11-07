db876411f5f66c9e556d9901f7dca2ed09194e47
Maharshee roy <m.roy@optonline.net>
2018-11-07 00:28:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(names)# code an argument here
   # Your code here
-  names.each_with_index{|value, 
+  names.each_with_index{|value, index|}
 end
 
 def summon_captain_planet# code an argument here
